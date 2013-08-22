3b61ff40788744d31f5065ced4ed94c8c3b6e08b
Thomas Roger <troger.solutec@tf1.fr>
2013-08-22 14:42:00 +0200

    ajout d'alias

--- alias.sh
+++ alias.sh
@@ -6,20 +6,28 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
+alias -- -='cd -'
 
 alias c='clear'
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
 alias ls='ls -G'
 alias l='ls -lhG'
 alias ll='ls -halG'
+alias lsd='ls -lFG | grep "^d"'
 
 alias kk='ll'
 alias mm='ll'
 
+alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+alias week='date +%V'
+
 # Alias commande DEV
 
 alias e='open -a /Volumes/Macintosh\ HD/Applications/Sublime\ Text\ 2.app'
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update'
 alias wr='cd /Users/troger/webroot && ll'
+
 
 # APACHE 
 alias apache_stop='sudo apachectl stop'
@@ -30,6 +38,10 @@ alias access='tail -f /private/var/log/apache2/access_log'
 alias error='tail -f /private/var/log/apache2/error_log'
 
 alias vh='cd /etc/apache2/sites/'
+
+# trafic HTTP
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 # GIT
 alias a='git add'
